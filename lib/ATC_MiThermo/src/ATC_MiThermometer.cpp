f9fea5efe7af689a99546c0e2427f03258eac2bd
Martin Elkjaer <martin@elkjaer-it.dk>
2022-09-18 13:42:24 +0200

    Load data from 2 sensor types

--- lib/ATC_MiThermo/src/ATC_MiThermometer.cpp
+++ lib/ATC_MiThermo/src/ATC_MiThermometer.cpp
@@ -1,191 +1,250 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// ATC_MiThermometer.cpp
-//
-// Bluetooth low energy thermometer/hygrometer sensor library for ESP32.
-// For sensors running ATC_MiThermometer firmware (see https://github.com/pvvx/ATC_MiThermometer)
-//
-// https://github.com/matthias-bs/ESP32_ATC_MiThermometer_Library
-//
-// Based on:
-// ---------
-// ESP32 BLE for Arduino (https://github.com/espressif/arduino-esp32/tree/master/libraries/BLE)
-// LYWSD03MMC.py by JsBergbau (https://github.com/JsBergbau/MiTemperature2)
-//
-// created: 05/2022
-//
-//
-// MIT License
-//
-// Copyright (c) 2022 Matthias Prinke
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
-// History:
-//
-// 20220521 Created
-// 20220527 Changed to a class/into a library
-//
-// ToDo: 
-// -
-//
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
 #include "ATC_MiThermometer.h"
-
 
 /*!
  * \class MyAdvertisedDeviceCallbacks
- * 
+ *
  * \brief Callback for advertised device found during scan
  */
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
     void onResult(BLEAdvertisedDevice advertisedDevice)
-    {      
-        if (advertisedDevice.haveServiceData()) {
+    {
+        if (advertisedDevice.haveServiceData())
+        {
             char buff[20];
             int datalen;
 
             datalen = (*advertisedDevice.getServiceDataUUID().getNative()).len;
             memcpy(buff, &(*advertisedDevice.getServiceDataUUID().getNative()).uuid, datalen);
-            //Serial.printf("UUID Len  %d \n", (*advertisedDevice.getServiceDataUUID().getNative()).len);
-            //Serial.printf("Service Data UUID length %d\n", datalen);
+            // Serial.printf("UUID Len  %d \n", (*advertisedDevice.getServiceDataUUID().getNative()).len);
+            // Serial.printf("Service Data UUID length %d\n", datalen);
             DEBUG_PRINT(">>>> ServiceDataUUID 0x");
-            for (int i = 0; i < datalen; i++) {
+            for (int i = 0; i < datalen; i++)
+            {
                 DEBUG_PRINT((buff[i] < 16) ? "0" : "");
                 DEBUG_PRINT(buff[i], HEX);
             }
             DEBUG_PRINTLN();
-            
+
             // Gets full length
-            //DEBUG_PRINT(">>>> ServiceDataUUID 0x");
-            //DEBUG_PRINT(advertisedDevice.getServiceDataUUID().toString().c_str());
-            
-            if (BLEUUID((uint16_t)0x181a).equals(advertisedDevice.getServiceDataUUID())) {
+            // DEBUG_PRINT(">>>> ServiceDataUUID 0x");
+            // DEBUG_PRINT(advertisedDevice.getServiceDataUUID().toString().c_str());
+
+            if (BLEUUID((uint16_t)0x181a).equals(advertisedDevice.getServiceDataUUID()))
+            {
                 datalen = advertisedDevice.getServiceData().length();
                 memcpy(buff, advertisedDevice.getServiceData().c_str(), datalen);
                 DEBUG_PRINTLN("Found a ServiceDataUUID for Temperature");
                 DEBUG_PRINT(">>>>  Service Data ");
-                for (int i = 0; i < datalen; i++) {
+                for (int i = 0; i < datalen; i++)
+                {
                     DEBUG_PRINT((advertisedDevice.getServiceData().c_str()[i] < 16) ? "0x0" : "0x");
                     DEBUG_PRINT(advertisedDevice.getServiceData().c_str()[i], HEX);
                     DEBUG_PRINT(" ");
                 }
                 DEBUG_PRINTLN();
 
-                if (advertisedDevice.haveName()) {
+                if (advertisedDevice.haveName())
+                {
                     DEBUG_PRINT("Name: ");
                     DEBUG_PRINTLN(advertisedDevice.getName().c_str());
                 }
-                if (advertisedDevice.haveManufacturerData()) {
+                if (advertisedDevice.haveManufacturerData())
+                {
                     DEBUG_PRINT("Manufacturer Data: ");
                     DEBUG_PRINTLN(advertisedDevice.getManufacturerData().c_str());
                 }
                 DEBUG_PRINT("Device Address: ");
                 DEBUG_PRINTLN(advertisedDevice.getAddress().toString().c_str());
-           } else {
+            }
+            else
+            {
                 DEBUG_PRINTLN("ServiceDataUUID doesn't match");
-           }
+            }
         } // if (advertisedDevice.haveServiceData())
-    } // void onResult(BLEAdvertisedDevice advertisedDevice)
+    }     // void onResult(BLEAdvertisedDevice advertisedDevice)
 };
-
 
 // Set up BLE scanning
 void ATC_MiThermometer::begin(void)
 {
     BLEDevice::init("");
-    _pBLEScan = BLEDevice::getScan(); //create new scan
+    _pBLEScan = BLEDevice::getScan(); // create new scan
     _pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    _pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
+    _pBLEScan->setActiveScan(false); // active scan uses more power, but get results faster
     _pBLEScan->setInterval(100);
-    _pBLEScan->setWindow(99);  // less or equal setInterval value
+    _pBLEScan->setWindow(99); // less or equal setInterval value
 }
 
-
 // Get sensor data by running BLE device scan
-unsigned ATC_MiThermometer::getData(uint32_t duration) {
+unsigned ATC_MiThermometer::getData(uint32_t duration)
+{
     BLEScanResults foundDevices = _pBLEScan->start(duration, false /* is_continue */);
 
-    // Initialize data vector    
-    std::vector<BLEAdvertisedDevice> supportedDevices = countSupportedDevices(foundDevices);
-    DEBUG_PRINTF("Supported devices found: %d\n", supportedDevices.size());            
+    // Initialize data vector
+    std::vector<BLEAdvertisedDevice> supportedDevices = getSupportedDevices(foundDevices);
+    DEBUG_PRINTF("Supported devices found: %d\n", supportedDevices.size());
+
     data.resize(supportedDevices.size());
 
-    // Known bug in Arduino BLE library - fix available:
-    // "ESP32 BLE scan, example works but devices found is always 0"
-    // https://forum.arduino.cc/t/esp32-ble-scan-example-works-but-devices-found-is-always-0/876703    
-    Serial.printf("Iteration: %d \n", foundDevices.getCount());
+    Serial.printf("Iteration: %d \n", supportedDevices.size());
 
-    for (unsigned i=0; i< foundDevices.getCount(); i++) {
-        
+    for (unsigned i = 0; i < supportedDevices.size(); i++)
+    {
+        DEBUG_PRINT("Found: ");
+        DEBUG_PRINTLN(supportedDevices[i].getAddress().toString().c_str());
         // Skip devices with wrong ServiceDataUUID
-        if (!BLEUUID((uint16_t)0x181a).equals(foundDevices.getDevice(i).getServiceDataUUID()))
-            continue;
-        
-        // Match all devices found against list of known sensors
-        for (unsigned n = 0; n < supportedDevices.size(); n++) {
-            DEBUG_PRINT("Found: ");
-            DEBUG_PRINT(foundDevices.getDevice(i).getAddress().toString().c_str());
-                
-            data[n].valid = true;
-            data[n].name = foundDevices.getDevice(i).getAddress().toString();
-        
+        if (BLEUUID((uint16_t)0x181a).equals(supportedDevices[i].getServiceDataUUID()))
+        {
+
+            data[i].valid = true;
+            data[i].name = supportedDevices[i].getAddress().toString();
+
             // Temperature
-            int temp_msb = foundDevices.getDevice(i).getServiceData().c_str()[7];
-            int temp_lsb = foundDevices.getDevice(i).getServiceData().c_str()[6];
-            data[n].temperature = (temp_msb << 8) | temp_lsb;
+            int temp_msb = supportedDevices[i].getServiceData().c_str()[7];
+            int temp_lsb = supportedDevices[i].getServiceData().c_str()[6];
+            data[i].temperature = (temp_msb << 8) | temp_lsb;
 
             // Humidity
-            int hum_msb = foundDevices.getDevice(i).getServiceData().c_str()[9];
-            int hum_lsb = foundDevices.getDevice(i).getServiceData().c_str()[8];
-            data[n].humidity = (hum_msb << 8) | hum_lsb;
+            int hum_msb = supportedDevices[i].getServiceData().c_str()[9];
+            int hum_lsb = supportedDevices[i].getServiceData().c_str()[8];
+            data[i].humidity = (hum_msb << 8) | hum_lsb;
 
             // Battery voltage
-            int volt_msb = foundDevices.getDevice(i).getServiceData().c_str()[11];
-            int volt_lsb = foundDevices.getDevice(i).getServiceData().c_str()[10];
-            data[n].batt_voltage = (volt_msb << 8) | volt_lsb;
+            int volt_msb = supportedDevices[i].getServiceData().c_str()[11];
+            int volt_lsb = supportedDevices[i].getServiceData().c_str()[10];
+            data[i].batt_voltage = (volt_msb << 8) | volt_lsb;
 
             // Battery state [%]
-            data[n].batt_level = foundDevices.getDevice(i).getServiceData().c_str()[12];         
-        
+            data[i].batt_level = supportedDevices[i].getServiceData().c_str()[12];
+
             // Received Signal Strength Indicator [dBm]
-            data[n].rssi = foundDevices.getDevice(i).getRSSI();       
+            data[i].rssi = supportedDevices[i].getRSSI();
+        }
+        else
+        {
+            data[i].valid = false;
+
+            std::string serviceData = supportedDevices[i].getServiceData();
+            int serviceDataLength = serviceData.length();
+            String returnedString = "";
+            for (int i = 0; i < serviceDataLength; i++)
+            {
+                int a = serviceData[i];
+                if (a < 16)
+                {
+                    returnedString = returnedString + "0";
+                }
+                returnedString = returnedString + String(a, HEX);
+            }
+
+            char service_data[returnedString.length() + 1];
+            returnedString.toCharArray(service_data, returnedString.length() + 1);
+            service_data[returnedString.length()] = '\0';
+
+            int data_length = 0;
+            int offset = 0;
+
+            switch (service_data[27 + offset])
+            {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+                data_length = ((service_data[27 + offset] - '0') * 2);
+                DEBUG_PRINTF("Valid data_length: %d\n", data_length);
+                break;
+            default:
+                //   Log.trace(F("Invalid data_length, not enriching the device data" CR));
+                continue;
+            }
+
+            double value = 9999;
+            value = value_from_service_data(service_data, 28 + offset, data_length);
+
+            switch (service_data[23 + offset])
+            {
+            case 'a':
+                data[i].name = supportedDevices[i].getAddress().toString();
+
+                data[i].batt_level = value;
+                data[i].rssi = supportedDevices[i].getRSSI();
+                data[i].valid = true;
+
+                // sendBattery(macAddress, (double)value, advertisedDevice.getRSSI(), timestamp);
+                break;
+            case 'd':
+                // humidity
+                {
+                    data[i].name = supportedDevices[i].getAddress().toString();
+
+                    value = value_from_service_data(service_data, 28 + offset, 4);
+                    data[i].temperature = (double)value / 10 * 100;
+
+                    value = value_from_service_data(service_data, 32 + offset, 4);
+                    data[i].humidity = (double)value / 10 * 100;
+
+                    data[i].rssi = supportedDevices[i].getRSSI();
+                    data[i].valid = true;
+
+                    // sendTempAndHumidity(macAddress, temperature, humidity, advertisedDevice.getRSSI(), timestamp);
+                    break;
+                }
+            default:
+                continue;
+            }
         }
     }
     return supportedDevices.size();
 }
 
-std::vector<BLEAdvertisedDevice> ATC_MiThermometer::countSupportedDevices(BLEScanResults devices){
-    int supportedDevices = 0;
+double ATC_MiThermometer::value_from_service_data(const char *service_data, int offset, int data_length)
+{
+    char rev_data[data_length + 1];
+    char data[data_length + 1];
+    memcpy(rev_data, &service_data[offset], data_length);
+    rev_data[data_length] = '\0';
+
+    // reverse data order
+    revert_hex_data(rev_data, data, data_length + 1);
+    double value = strtol(data, NULL, 16);
+    if (value > 65000 && data_length <= 4)
+        value = value - 65535;
+    return value;
+}
+
+void ATC_MiThermometer::revert_hex_data(char *in, char *out, int l)
+{
+    // reverting array 2 by 2 to get the data in good order
+    int i = l - 2, j = 0;
+    while (i != -2)
+    {
+        if (i % 2 == 0)
+            out[j] = in[i + 1];
+        else
+            out[j] = in[i - 1];
+        j++;
+        i--;
+    }
+    out[l - 1] = '\0';
+}
+
+std::vector<BLEAdvertisedDevice> ATC_MiThermometer::getSupportedDevices(BLEScanResults devices)
+{
     std::vector<BLEAdvertisedDevice> result;
-    for (unsigned i=0; i< devices.getCount(); i++) {
-        DEBUG_PRINTLN("Counting...");
-        
-        // Skip devices with wrong ServiceDataUUID
-        if (BLEUUID((uint16_t)0x181a).equals(devices.getDevice(i).getServiceDataUUID())) {
+    for (unsigned i = 0; i < devices.getCount(); i++)
+    {
+        if (BLEUUID((uint16_t)0x181a).equals(devices.getDevice(i).getServiceDataUUID()))
+        {
             result.push_back(devices.getDevice(i));
-            
-        } else if (BLEUUID((uint16_t)0xfe95).equals(devices.getDevice(i).getServiceDataUUID())){
-            
+        }
+        else if (BLEUUID((uint16_t)0xfe95).equals(devices.getDevice(i).getServiceDataUUID()))
+        {
             result.push_back(devices.getDevice(i));
-        } else {
-            
+        }
+        else
+        {
+            // Unsupported device
         }
     }
     return result;
@@ -194,7 +253,12 @@ std::vector<BLEAdvertisedDevice> ATC_MiThermometer::countSupportedDevices(BLESca
 // Set all array members invalid
 void ATC_MiThermometer::resetData(void)
 {
-    //for (int i=0; i < _known_sensors.size(); i++) {
-    //    data[i].valid = false;
-    //}
+    for (int i = 0; i < data.size(); i++)
+    {
+        data[i].valid = false;
+        data[i].temperature = 0;
+        data[i].batt_level = 0;
+        data[i].humidity = 0;
+        data[i].batt_voltage = 0;
+    }
 }
