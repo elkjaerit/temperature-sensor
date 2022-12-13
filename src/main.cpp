3528c50920a9c57708bd37da2edd9dce05d4a57f
Martin Elkjær <martin@elkjaer-it.dk>
2022-12-13 10:28:25 +0100

    Fixed connection issues.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include "jsonutils.h"
 #include "publisher.h"
 
-const int scanTime = 15; // BLE scan time in seconds
+const int scanTime = 30; // BLE scan time in seconds
 
 ATC_MiThermometer miThermometer;
 
@@ -19,8 +19,23 @@ SignerConfig config;
 
 String response = "";
 
-uint32_t resetAfterMillis = 12 * 60 * 60 * 1000; // Reset after 12 hours.
+uint32_t resetAfterMillis = 120 * 60 * 1000; // Reset after 120 minutes.
 uint32_t lastResetWas;
+
+void tokenStatusCallback(TokenInfo info)
+{
+  if (info.status == esp_signer_token_status_error)
+  {
+    Serial.printf("Token info: type = %s, status = %s\n", Signer.getTokenType(info).c_str(), Signer.getTokenStatus(info).c_str());
+    Serial.printf("Token error: %s\n", Signer.getTokenError(info).c_str());
+  }
+  else
+  {
+    Serial.printf("Token info: type = %s, status = %s\n", Signer.getTokenType(info).c_str(), Signer.getTokenStatus(info).c_str());
+    if (info.status == esp_signer_token_status_ready)
+      Serial.printf("Token: %s\n", Signer.accessToken().c_str());
+  }
+}
 
 void setup()
 {
@@ -46,14 +61,15 @@ void setup()
   // then goes into a blocking loop awaiting configuration and will return success result
 
   bool res;
-  // res = wm.autoConnect(); // auto generated AP name from chipid
-  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+
+  wm.setConfigPortalTimeout(180);
+
   res = wm.autoConnect("SmartHeatingAP"); // password protected ap
 
   if (!res)
   {
     Serial.println("Failed to connect");
-    // ESP.restart();
+    ESP.restart();
   }
   else
   {
@@ -70,7 +86,8 @@ void setup()
     config.service_account.data.client_email = "esp-temp-sensor@smart-heating-1.iam.gserviceaccount.com";
     config.service_account.data.project_id = "smart-heating-1";
     config.service_account.data.private_key = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDBU44J69xWE3IU\nmrh0Qon7z4gSWPqrjkKJFIyfLuHaSOg/F/WhSWTJLph+QfmRK5jZJ9E2FVIDWYyZ\nzWIK7tyJ/MWclv50csbGJxe6zcL6Gve5qZG96hDN2+IM4h1wEbrBMsJbPspwJwQF\n/Y4j41NoWIxf2YWS0BFbEBRaQckmPi3W1XhtMz38K/1C7E0uyFJ70B+EK1/DUGJc\n0t1drRdDOT5Zqse94Y+oQiU6EmginAXYQuEgUKWrDMktT7W9HoCKNFB94p/2+0Wu\nJUVGjtPsgu0ZyHr5rGEIXq5EZZGb4x8Xj6Yy8BWgx5TWfs4EHc32zOvMi2pMILJR\nlWgKvQUdAgMBAAECggEAHXglILIJZj2UmDt1fpNeATqDXXiezWPkFI1DWkiFOQcA\nEA4Q7UMIz/oAWM6pwk1JAWEmbP4XAFe4LQG3vj7m80nKvbHum8WbMz1mSp1u6T0P\nIggNJUv2v8qSzK/zGNA7DZQ1OD98EFiCLn+WGTZf0noc+8dmqxh4bM8oqxF4HhH8\nJHvlpiuPXvO//QxccGxnfRNdZCTZFfqdBMPVy/JCwuMBJwUWjb40KCLmOquMnxO2\nof35jhzLZyZPqnoDc+9Urz3itJ0n6LlDvUHHdnUXTtlsJiDfgmUBHEp4GfRvuzDG\n/n/vNEyERmFtlUXd7dj71GxXlroejuSDiYw90a/EwQKBgQD4Hhv09+7KNon4L8fJ\naJKowBl9VlCrwD4ogNIXhHuc9UpENl88Z/lvweEuS1wFRnTXlk4aslnqCf6wGk0n\ngli9FnrgnAIQV7cO6CoFXE40cr3A8qGW1gvXRNeycT82Lbre9p3VDEc7qlUYwtgS\nAWApi+Rro7H2doEZ/EjiMX7zFQKBgQDHd9btpiFOPjLHzx+OdzW3bkA7LTuwtfId\n/qCCM83TwnwfPj9BQZeCSym01FLf2IFxC6pesdzdC964jEdWKXsTFPI5iw+lxAjn\nhotzxncBrLH6mr+hEe6oYPtdf7Lyh0794RztSC9fCghlQIKfa3mWv6xh8feBCAb1\nMuo9Ojhr6QKBgAyhO7sUg8s3S36esATIle6RGLsQkbqsZn2ZURhxXfl3Yvhl7CMf\nB8twiw3YOC7sjzYKKJ+jRIBtUdGVBShlsi6t3kNgrZo3XNIdb0YmlLSGwrH3p9IN\nwzyJ/JDQwu73FPQUiaQ2o8mdugcwo98GwuZagJ4aDw0Eqz2vYeZhrpyhAoGAMpNP\nOriZz0X0CDebL7tIMndQ7/A7J0Yq2rIaLolGEgBJCn70+O3Rpaa7L296h+lUgL5N\nd28vqhh9Y1umJtGjtCXmePKqiARZoaK6ryNOP21zPzCHkCaE1cqpUX60d8wZGkQ+\n2mcoI7fegxXFSPiuPqvg4IDCmcPYR8meHOIrNikCgYEAgvId6EYpdVY97DI7njcO\nIoGUGzDQdWiD6CfLsP3eliKXroHa89R60RhWkx/fTAi3qx1rdMwvtw2VR3fbPc+O\nKmfwwErZZ5xpS6ihB0/m3mUq8caE/GAiiJoF8xrUEcDAYtkZnB/Opln6nKKzbrAF\nO4oLCGAjpNCljMfHeM5uG0s=\n-----END PRIVATE KEY-----\n";
-
+    config.signer.expiredSeconds = 3600;
+    config.token_status_callback = tokenStatusCallback;
     /** Assign the API scopes (required)
      * Use space or comma to separate the scope.
      */
@@ -79,7 +96,7 @@ void setup()
     /* Create token */
     Signer.begin(&config);
 
-    esp_task_wdt_init(scanTime + 5, true);
+    esp_task_wdt_init(scanTime + 10, true);
     esp_task_wdt_reset();
 
     miThermometer.begin();
@@ -91,18 +108,21 @@ void setup()
 void loop()
 {
 
+  esp_task_wdt_add(NULL);
+  esp_task_wdt_reset();
+
+  Serial.printf("Heap size: %i \n", ESP.getHeapSize());
+  Serial.printf("Free heap size: %i \n", ESP.getFreeHeap());
+  Serial.printf("Max alloc heap size: %i \n", ESP.getMaxAllocHeap());
+
   /* Check for token generation ready state and also refresh the access token if it expired */
   bool ready = Signer.tokenReady();
   if (ready)
   {
-    esp_task_wdt_add(NULL);
-    esp_task_wdt_reset();
-
     Serial.println("Start scanning... - gateway id: " + WiFi.macAddress());
 
-    // Set sensor data invalid
+    // Get sensor data
     miThermometer.resetData();
-
     miThermometer.getData(scanTime);
 
     String sensorDataAsJson = buildJson(miThermometer.data);
@@ -117,12 +137,17 @@ void loop()
     String access_token = Signer.accessToken().c_str();
     send(sensorDataAsJson, access_token);
   }
+  else
+  {
+    Serial.println("Token not ready");
+  }
 
   esp_task_wdt_reset();
   uint32_t now = millis();
   if (now >= lastResetWas + resetAfterMillis)
   {
-    lastResetWas = now;    
-    resetESP();
-  }  
+    lastResetWas = now;
+    resetESP();    
+  }
+  delay(scanTime * 1000);
 }
